8a37d5dc6ca6678c13c76522979d2c109c879a75
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-29 20:42:54 +0200

    upd

--- Lesson8_Graph/MainWindow.xaml.cs
+++ Lesson8_Graph/MainWindow.xaml.cs
@@ -16,6 +16,47 @@ namespace Lesson8_Graph
         public MainWindow()
         {
             InitializeComponent();
+
+            Rectangle rectangle = new Rectangle
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Height = 100,
+                Width = 100,
+                Fill = Brushes.Red
+            };
+
+            Grid.SetColumn(rectangle, 1);
+            Grid.SetRow(rectangle, 0);
+            grid.Children.Add(rectangle);
+
+            Ellipse ellipse = new Ellipse
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Height = 100,
+                Width = 100,
+                Fill = Brushes.Green
+            };
+
+            Grid.SetColumn(ellipse, 1);
+            Grid.SetRow(ellipse, 1);
+            grid.Children.Add(ellipse);
+
+            Polygon polygon = new Polygon
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Fill = Brushes.Blue
+            };
+
+            polygon.Points.Add(new Point(180, 100));
+            polygon.Points.Add(new Point(135, 30));
+            polygon.Points.Add(new Point(80, 100));
+
+            Grid.SetColumn(polygon, 1);
+            Grid.SetRow(polygon, 2);
+            grid.Children.Add(polygon);
         }
     }
 }
